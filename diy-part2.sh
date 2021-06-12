9d2f83e6c8f3f88549bc647a7b5c7384b9b8fab5
wggglggg <72190085+wggglggg@users.noreply.github.com>
2021-06-12 09:17:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/wggglggg/luci-app-poweroff package/luci-app-poweroff
+git clone https://github.com/wggglggg/luci-app-poweroff  package/luci-app-poweroff
+git clone https://github.com/wggglggg/luci-app-poweroff  lede/package/luci-app-poweroff
